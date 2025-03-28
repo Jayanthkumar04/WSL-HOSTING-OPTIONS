5983589ce8bb293241d7949b2e669d056c934196
Jayanthkumar04 <jayanthkumar598@gmail.com>
2025-03-28 12:18:11 +0530

    Add project files.

---
+++ AddServiceClient/WebForm1.aspx.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.UI;
+using System.Web.UI.WebControls;
+using AddServiceClient.AddServiceReference;
+namespace AddServiceClient
+{
+    public partial class WebForm1 : System.Web.UI.Page
+    {
+        protected void Page_Load(object sender, EventArgs e)
+        {
+
+        }
+
+        protected void Button1_Click(object sender, EventArgs e)
+        {
+            Service1Client client = new Service1Client("BasicHttpBinding_IService1");
+            Response.Write(client.Addition(5, 5));
+        }
+    }
+}
---
+++ Console-Host/Program.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using AddService;
+using System.ServiceModel;
+using System.ServiceModel.Description; 
+namespace Console_Host
+{
+    public class Program
+    {
+        static void Main(string[] args)
+        {
+            Uri baseAddress = new Uri("http://localhost:6001");
+
+            ServiceHost sh = new ServiceHost(typeof(Service1), baseAddress);
+
+            ServiceEndpoint se = sh.AddServiceEndpoint(typeof(IService1), new BasicHttpBinding(), baseAddress);
+
+            ServiceMetadataBehavior smb = new ServiceMetadataBehavior();
+            smb.HttpGetEnabled = true;
+            sh.Description.Behaviors.Add(smb);
+
+            sh.Open();
+            Console.WriteLine("Service is ready.......");
+
+            Console.ReadLine();
+
+            sh.Close();
+
+
+        }
+    }
+}
